28994081785e2c6a37f61c9891e91d84fd1f64c9
linbc <linbc@zju.edu.cn>
2022-03-09 22:55:54 +0800

    topk problem

---
+++ src/main/java/TopK.java
@@ -0,0 +1,77 @@
+public class TopK {
+    /**
+     * top k 算法：
+     * 首先，构建最小堆
+     * 后续遍历时，每一个数和最小堆的顶比较，若较大，则pop出最小堆顶，压入当前数字。
+     */
+
+    int[] array = new int[1024];
+    int index;
+    int maxValue ;
+    public TopK(int n){
+        maxValue = n;
+        index = 0;
+    }
+
+    public static void main(String[] args) {
+        TopK h = new TopK(3);
+        int[] nums = new int[]{1,5,23,8,7,4,99};
+
+        int k = 3;
+        for(int i=0;i<k;i++){
+            h.push(nums[i]);
+        }
+        for(int i=k;i<nums.length;i++){
+            if(h.peek()<nums[i]){
+                h.pop();
+                h.push(nums[i]);
+            }
+        }
+
+        return;
+
+
+
+    }
+
+
+
+    public void push(int n){
+        array[index] = n;
+        int cur = index;
+        while(array[cur]<array[(cur-1)/2]){
+            int temp = array[cur];
+            array[cur] = array[(cur-1)/2];
+            array[(cur-1)/2] = temp;
+            cur = (cur-1)/2;
+        }
+        index++;
+    }
+
+    public int peek(){
+        return array[0];
+    }
+
+    public void pop(){
+        array[0] = array[index-1]; // 获取最后一位数，放在根处
+        index--;
+        int res = 0;
+        while((array[res]>array[res*2+1] && res*2+1 < index )
+                || (array[res]>array[res*2+2] && res*2+2 < index)){
+            if(array[res*2+1]<array[res*2+2]){
+                int temp = array[res];
+                array[res] = array[res*2+1];
+                array[res*2+1] = temp;
+                res = res*2+1;
+            }
+            else{
+                int temp = array[res];
+                array[res] = array[res*2+2];
+                array[res*2+2] = temp;
+                res = res*2+2;
+            }
+        }
+
+
+    }
+}
